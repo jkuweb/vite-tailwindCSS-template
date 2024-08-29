2c4dece8e485648244faf98e23ece980af9f0d29
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2024-08-29 21:02:29 +0200

    chore(prettier): run prettier script

--- src/index.tsx
+++ src/index.tsx
@@ -5,29 +5,29 @@ import './style.scss';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(
-    <>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>API_URL</th>
-              <th>MODE</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{config.API_BASE}</td>
-              <td>{config.MODE}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-      <div className="container">
-        <h1 className="background">REACT TEMPLATE!</h1>
-        <App />
-      </div>
-    </>
-  );
+	const root = createRoot(rootElement);
+	root.render(
+		<>
+			<div>
+				<table>
+					<thead>
+						<tr>
+							<th>API_URL</th>
+							<th>MODE</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>{config.API_BASE}</td>
+							<td>{config.MODE}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div className='container'>
+				<h1 className='background'>REACT TEMPLATE!</h1>
+				<App />
+			</div>
+		</>
+	);
 }
