cff9c0891ee1004d8d681074b8403818e7433f1f
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2024-08-29 21:00:49 +0200

    chore(eslint config file): include postcss file in ignore files

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 export default defineConfig({
-  plugins: [checker({ typescript: true }), react()],
-  esbuild: {
-    jsxInject: `import React from 'react'`,
-  },
+	plugins: [checker({ typescript: true }), react()],
+	esbuild: {
+		jsxInject: `import React from 'react'`,
+	},
 });
